fd8dc9c45fc510ad8a799bee4696e75381e6dd8d
Sergey Nikolaychuk <30438484+NikolasSerg@users.noreply.github.com>
2021-06-17 17:20:59 +0300

    connected to mongoDb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/auth");
 const analyticsRoutes = require("./routes/analytics");
@@ -8,9 +9,19 @@ const categoryRoutes = require("./routes/category");
 const orderRoutes = require("./routes/order");
 const positionRoutes = require("./routes/position");
 
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI)
+    .then(() => {
+        console.log('Mongo connected')
+    }).
+    catch((err) => {
+        console.error(err)
+});
+
 app.use(require('morgan')('dev'));
 app.use(require('cors')());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
@@ -19,4 +30,4 @@ app.use('/api/categoryRoutes', categoryRoutes);
 app.use('/api/orderRoutes', orderRoutes);
 app.use('/api/positionRoutes', positionRoutes);
 
-module.exports =  app;
+module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://mongoSergiu:mongoSergiuMEAN@cluster0.lbufu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
