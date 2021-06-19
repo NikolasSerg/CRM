b26cbec9d572dae092963950774888d64cf48081
Sergey Nikolaychuk <30438484+NikolasSerg@users.noreply.github.com>
2021-06-19 15:04:03 +0300

    to create a new user via mongoose

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,10 +9,25 @@ module.exports.login = function (req, res) {
     })
 };
 
-module.exports.register = function (req, res) {
-    let user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save().then(() => console.log('user  created'))
-};
+
+module.exports.register = async function (req, res) {
+    let candidate = await User.findOne({email: req.body.email})
+    if(candidate) {
+        res.status(409).json({
+            message: "this email is exist"
+        })
+    } else {
+        const user = new User({
+          email: req.body.email,
+          password: req.body.password
+        })
+        try {
+          await user.save();
+          res.status(200).json(user);
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+  };
+  
