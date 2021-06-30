ef88f2013613d2118cbce30fcce762dba93b609f
NikolasSerg <nikolasalon@gmail.com>
2021-06-30 10:18:04 +0300

    some change

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,7 +17,7 @@ module.exports.login = async function (req, res) {
                 {expiresIn: 60 * 60}
             );
             res.status(200).json({
-                token: `Barer ${token}`
+                token: `Bearer ${token}`
             })
        } else {
            res.status(401).json({
