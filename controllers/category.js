c9e783b747bd3a4e00dc5d39d9de58217bda3cb7
sn <nikolasalon@gmail.com>
2021-07-04 20:31:51 +0300

    created upload for image

--- controllers/category.js
+++ controllers/category.js
@@ -1,14 +1,43 @@
-module.exports.getAll = function (req, res) {
-    res.json({message: "categories"})
-}
-module.exports.getById = function (req, res) {
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandler');
+const Position = require('../models/Position');
 
-}
-module.exports.remove = function (req, res) {
 
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({
+            user: req.user.id
+        });
+        res.status(200).json(categories)
+    } catch (error) {
+        errorHandler(res, error)
+    }
+}
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category)
+    } catch (error) {
+        errorHandler(res, error)
+    }
+}
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+        res.status(200).json({
+            message: 'all were deleted'
+        })
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 module.exports.update = function (req, res) {
 
